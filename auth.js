32b07d82182c421a6b078c1b85afe387272da985
Marco Galizzi <marcogalizzi89@gmail.com>
2018-09-29 15:38:27 +0200

    style

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authorize = async (ctx, next) => {
   const headerCookie = ctx.cookies.get('pulsehunt_cookie');
-  const decoded = jwt.verify(headerCookie, 'ramen', (err, res) => {
+  jwt.verify(headerCookie, 'ramen', (err, res) => {
     if (err) {
       console.log('error: ', err);
       ctx.body = 'log in needed to access restricted area';
